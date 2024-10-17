7fbb1a7d6a7168225d4536d62ab62b53739363a2
Aerry Asmani <aerryasmani@gmail.com>
2024-10-17 21:05:01 +0800

    Added QR cards functions

--- Test/System Testing/UI Test/SJ_11_UI-ViewQR.py
+++ Test/System Testing/UI Test/SJ_11_UI-ViewQR.py
@@ -10,7 +10,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-class Btn_MuatnaiK(unittest.TestCase):
+class Btn_Share(unittest.TestCase):
 
     def setUp(self):
         # Set up your WebDriver (in this case, Chrome)
@@ -19,22 +19,23 @@ class Btn_MuatnaiK(unittest.TestCase):
         self.driver.get("http://sedekahje.com")
         logger.info("Browser opened and navigated to SedekahJe")
     
-    def test_btn_is_present(self):
+    def test_ViewQR_icon_is_present(self):
         driver = self.driver
         # Wait for the button to be present
         wait = WebDriverWait(driver, 30)
     
         # Locate the button by XPath or other selector (you can adjust the locator as needed)
-        btn = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/section/footer/a[2]/button")))
+        btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/section/div[3]/div[1]/div/div/div[3]/button[3]")))
     
         # Verify the button is displayed
         self.assertTrue(btn.is_displayed(), "Button is not displayed")
-    
-        # Verify the button text (for different screen sizes)
-        button_text = btn.find_element(By.CSS_SELECTOR, "body > section > footer > a:nth-child(2) > button > p").text
-        self.assertEqual(button_text, "Muatnaik QR", "Button text is incorrect")
-        logger.info("Button is present and visible")
+        logger.info("Icon is displaying")
 
+        # Verify the 'data-active' attribute
+        expected_data_active = "closed"  # Change to "true" if you expect it to be true when active
+        actual_data_active = btn.get_attribute("data-state")
+        self.assertEqual(actual_data_active, expected_data_active, "Button 'data-active' attribute is incorrect")
+    
     def tearDown(self):
         # Close the browser after the test is done
         self.driver.quit()
